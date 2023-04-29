c308bb11d0206e8a9f676c8defe3fb7ed2aba06d
512kbb <dieg.parraa@duocuc.cl>
2023-04-29 05:40:56 -0400

    heroku added files

--- index.js
+++ index.js
@@ -61,5 +61,6 @@ app.use((request, response) => {
   response.status(404).json({ error: 'not found' })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
